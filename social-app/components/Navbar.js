71a770a80cc0eea558d347aad667f2ff4d0d1115
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-06-13 22:22:35 -0600

    Sign in and sign out buttons in navbar

--- social-app/components/Navbar.js
+++ social-app/components/Navbar.js
@@ -1,7 +1,31 @@
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Navbar = () => {
-  return (
+  const { data: session } = useSession()
+
+  if(session) {
+    return <>
+      <nav className="header">
+        <div className="logo">
+          <h1>logo</h1>
+        </div>
+        <div className="hello">
+          Hi {session.user.name} 
+        </div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/protected">
+          <a>Protected</a>
+        </Link>
+        <div className="signin">
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+      </nav>
+    </>
+  }
+  return <>
     <nav className="header">
       <div className="logo">
         <h1>logo</h1>
@@ -12,8 +36,12 @@ const Navbar = () => {
       <Link href="/protected">
         <a>Protected</a>
       </Link>
+      <div className="signin">
+        <button onClick={() => signIn()}>Sign in</button>
+      </div>
     </nav>
-  );
+  </>
+
 };
 
 export default Navbar;
